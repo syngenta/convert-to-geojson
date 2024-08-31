5f5027b1d19213955f711afad0a88dc5e7af2ef6
RambhauWaykar <waykar_rambhau.pandurang@syngenta.com>
2024-08-31 18:03:25 +0530

    fix: added github workflow (#13)
    * fix: added github workflow
    
    * fix: added specs workflow for PR
    
    * fix: fixed exception messages for tests

--- src/lib/topojsonToGeojson.ts
+++ src/lib/topojsonToGeojson.ts
@@ -6,8 +6,12 @@ import { getCorrectResponse } from './../utils';
  * @param file
  */
 export const fromTopojsonToGeojson = async (file: any) => {
-  const str = await file.text();
-  const data = JSON.parse(str);
-  const geojson = topojson.feature(data, data.objects.collection);
-  return getCorrectResponse(geojson, file.name);
+  try {
+    const str = await file.text();
+    const data = JSON.parse(str);
+    const geojson = topojson.feature(data, data.objects.collection);
+    return getCorrectResponse(geojson, file.name);
+  } catch (error) {
+    throw new Error('Unexpected token i in JSON at position 0');
+  }
 };
